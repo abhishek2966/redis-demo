2e8294b4a79802e9aa5f62267b01ef52ba172fee
abhishek2966 <abhishek296@gmail.com>
2022-11-28 02:04:36 +0530

    changes

--- main.go
+++ main.go
@@ -5,33 +5,49 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v9"
 )
 
 const redisUrl = "localhost:6379"
 
 func main() {
 	redisOptions := &redis.Options{
-		Addr:         redisUrl,
-		MaxRetries:   3,
-		MinIdleConns: 50,
-		MaxConnAge:   30 * time.Minute,
-		IdleTimeout:  10 * time.Minute,
-		DB:           0, // use default DB
+		Addr: redisUrl,
+		DB:   0, // use default DB
 	}
 
-	client := redis.NewClient(redisOptions)
-	_, err := client.Ping().Result()
+	ctx := context.Background()
+
+	rdb := redis.NewClient(redisOptions)
+	_, err := rdb.Ping(ctx).Result()
 	if err != nil {
 		fmt.Println("ping err:", err)
 		return
 	}
-	defer client.Close()
-	ctx := context.Background()
-	err = redis.SAdd(ctx, "user:id:name", "user:1:John", "user:2:Jack").Err()
+	defer rdb.Close()
+
+	// working with redis Strings
+	//sets "reqCount:example.com" = 1 to be expired in an hour
+	err = rdb.Set(ctx, "reqCount:example.com", 1, time.Hour).Err()
 	if err != nil {
 		fmt.Println("redis addd err:", err)
 	}
-	redisValExists := redis.SIsMember(ctx, "user:id:name", "user:1:John").Val()
+	//increments value of "reqCount:example.com" by 1
+	rdb.Incr(ctx, "reqCount:example.com")
+
+	//increments value of "reqCount:example.com" by 4
+	rdb.IncrBy(ctx, "reqCount:example.com", 4)
+
+	//sets string user1 value user1@email.com
+	rdb.Set(ctx, "user1", "user1@email.com")
+
+	fmt.Println(rdb.Get(ctx, "reqCount:example.com").Result())
+	fmt.Println(rdb.Get(ctx, "reqCount:google.com").Result())
+
+	err = rdb.SAdd(ctx, "user:id:name", "user:1:John", "user:2:Jack").Err()
+	if err != nil {
+		fmt.Println("redis addd err:", err)
+	}
+	redisValExists := rdb.SIsMember(ctx, "user:id:name", "user:1:John").Val()
 	fmt.Println(redisValExists)
 }
